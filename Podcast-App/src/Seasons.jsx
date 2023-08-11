d04e405f5361f21a40808322e3cef9347db3ecc5
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-08-11 21:16:50 +0200

    Initial and loading states
    -Initial and loading states added

--- Podcast-App/src/Seasons.jsx
+++ Podcast-App/src/Seasons.jsx
@@ -5,11 +5,16 @@ export default function Seasons() {
   const params = useParams();
   const [show, setShow] = React.useState({});
   const [selectedSeason, setSelectedSeason] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
+    setLoading(true); // Set loading to true when starting a new fetch
     fetch(`https://podcast-api.netlify.app/id/${params.id}`)
       .then((response) => response.json())
-      .then((data) => setShow(data));
+      .then((data) => {
+        setShow(data);
+        setLoading(false); // Set loading to false once data is fetched
+      });
   }, [params.id]);
 
   const handleSeasonClick = (season) => {
@@ -21,35 +26,55 @@ export default function Seasons() {
   };
 
   return (
-    <div className="container text-center fw-light">
-      <h2>{show.title}</h2>
-      <p>{show.description}</p>
-      {show.seasons && show.seasons.length > 0 ? (
-        show.seasons.map((season) => (
-          <div key={season.season}>
-            <button type="button" className="btn btn-outline-light" onClick={() => handleSeasonClick(season)}>
-              <h4>{season.title}</h4>
-              <img src={season.image} width="20%" alt={season.title} />
-              {season.description}
-            </button>
-            {selectedSeason && selectedSeason.season === season.season && (
-              <div>
-                <h3>Episodes:</h3>
-                {selectedSeason.episodes.map((episode) => (
-                  <div key={episode.id}>
-                    <h3>{episode.title}</h3>
-                    <p>{episode.description}</p>
-                    <audio controls>
-                      <source src={episode.file} />
-                    </audio>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))
-      ) : (
+    <div className="container text-center ">
+      {loading ? (
         <h2>Loading.....</h2>
+      ) : (
+        <div>
+          <img src={show.image} width={"200rem"} alt={show.title} />
+          <h2>{show.title}</h2>
+          <p>{show.description}</p>
+          {show.seasons && show.seasons.length > 0 ? (
+            show.seasons.map((season) => (
+              <div key={season.season}>
+                <div className="d-grid gap-2 col-6 mx-auto">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => handleSeasonClick(season)}
+                  >
+                    <h4>Season {season.season}</h4>
+                    {season.description}
+                  </button>
+                </div>
+                {selectedSeason && selectedSeason.season === season.season && (
+                  <div>
+                    {selectedSeason.episodes.map((episode) => (
+                      <div key={episode.id}>
+                        <span>
+                          <img
+                            src={season.image}
+                            width={"30rem"}
+                            alt={episode.title}
+                          />
+                          <h3>
+                            {episode.episode}. {episode.title}
+                          </h3>
+                        </span>
+                        <p>{episode.description}</p>
+                        <audio controls>
+                          <source src={episode.file} />
+                        </audio>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <h2>No seasons available</h2>
+          )}
+        </div>
       )}
     </div>
   );
--- Podcast-App/src/components/HomePage.jsx
+++ Podcast-App/src/components/HomePage.jsx
@@ -4,20 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 import { Link } from "react-router-dom";
 
 export default function Data() {
-
-  const v4Id = uuidv4()
+  const v4Id = uuidv4();
 
   const [show, setShowData] = React.useState([]);
   const [favourites, setFavourites] = React.useState([]);
-
-
-
-
+  const [loading, setLoading] = React.useState(true); // New loading state
 
   React.useEffect(() => {
     fetch("https://podcast-api.netlify.app/shows")
       .then((response) => response.json())
-      .then((data) => setShowData(data));
+      .then((data) => {
+        setShowData(data);
+        setLoading(false); // Set loading to false once data is fetched
+      });
   }, []);
 
   const addFavouritesShow = (show) => {
@@ -29,63 +28,57 @@ export default function Data() {
     const date = new Date(dateString);
     const formattedDate = date.toLocaleDateString(undefined, {
       year: "numeric",
-      month: "long",
       day: "numeric",
+      month: "long",
     });
     return formattedDate;
   };
 
   return (
-<div>
-
-    {favourites.length > 0 && (
-      <div>
-        <h2>Favorites:</h2>
-        <ul>
-          {favourites.map((favShow) => (
-            <div key={favShow.v4Id}>
-            <img src={favShow.image} width={"200rem"} /> 
-              
-              
-              
-          </div>
-            
-          ))}
-        </ul>
-      </div>
-    )}
-
-    <div className="container">
-      <div className="row row-cols-2 row-cols-sm-2 row-cols-md-4 row-cols-lg-8">
-        {show.map((show) => (
-          <div key={show.id} className="col mb-3 d-flex">
-               
-            <div className="card flex-fill" style={{ width: "14rem" }}>
-            <Link to={`/${show.id}`}>
-              <img className="card-img-top rounded-0" src={show.image} alt="none" />
-              </Link>
-              <div className="card-body">
-               <h5 className="card-title">{show.title}</h5>
+    <div>
+      {favourites.length > 0 && (
+        <div>
+          <h2>Favorites:</h2>
+          <ul>
+            {favourites.map((favShow) => (
+              <div key={favShow.v4Id}>
+                <img src={favShow.image} width={"200rem"} />
               </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Seasons: {show.seasons}</li>
-                <li className="list-group-item"> Last updated: {formatUpdatedAt(show.updated)}</li>
-                <li className="list-group-item">
-                  <AddFavourites
-                    handleFavouritesClick={() => addFavouritesShow(show)}
-                    isFavorite={favourites.includes(show)}
-                  />
-                </li>
-                <li className="list-group-item"></li>
-              </ul>
-            </div>
-            
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {loading ? ( // Display loading indicator while data is being fetched
+        <h2>Loading.....</h2>
+      ) : (
+        <div className="container">
+          <div className="row row-cols-3 row-cols-sm-2 row-cols-md-4 row-cols-lg-8">
+            {show.map((show) => (
+              <div key={show.id} className="col mb-3 d-flex">
+                <div className="card flex-fill" style={{ width: "14rem" }}>
+                  <Link to={`/${show.id}`}>
+                    <img className="card-img-top rounded-0" src={show.image} alt="none" />
+                  </Link>
+                  <div className="card-body p-0 text-center">
+                    <h5 className="card-title">{show.title}</h5>
+                  </div>
+                  <ul className="list-group list-group-flush text-center fs-6">
+                    <li className="list-group-item p-0">Seasons: {show.seasons}</li>
+                    <li className="list-group-item p-0">Last updated: {formatUpdatedAt(show.updated)}</li>
+                    <li className="list-group-item p-0">
+                      <AddFavourites
+                        handleFavouritesClick={() => addFavouritesShow(show)}
+                        isFavorite={favourites.includes(show)}
+                      />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {/* Display the favorite show names */}
-     
-    </div>
+        </div>
+      )}
     </div>
   );
 }

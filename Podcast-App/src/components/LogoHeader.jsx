4e6f8fb4ac6a0c6654a131806a379be4f3f7f99c
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-08-16 04:47:25 +0200

    Latest to Oldest
    Favourites sort Latest to Oldest

--- Podcast-App/src/components/LogoHeader.jsx
+++ Podcast-App/src/components/LogoHeader.jsx
@@ -6,13 +6,15 @@ export default function LogoHeader () {
 
 return (
 <div className="d-flex justify-content-between align-items-center bg-black p-2">
+
+
 <img src="./src/assets/images/podcast.png"  width={"80px"} height={"80px"}/>
 
-<Link to={"/login"}>
-<button>Sign Out</button>
-</Link>
- <div>
 
+ <div>
+ <Link to={"/login"}>
+<button className="btn btn-primary btn-sm fs-7" >Sign Out</button>
+</Link>
 
 </div>
 </div>
